82224653de5d073f7b711abd7f90e81543c67b0f
Lucas Lucyk <llucyk@grupospec.com>
2023-12-28 15:47:34 -0300

    login on first window

--- src/pages/popup/Applications.tsx
+++ src/pages/popup/Applications.tsx
@@ -1,3 +1,6 @@
+import React, { useState } from 'react';
+import Login from '@root/src/pages/apps/Login';
+
 import '@pages/popup/Applications.css';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
@@ -6,10 +9,22 @@ import Chat from '@root/src/pages/apps/Chat';
 import Sftp from '@root/src/pages/apps/Sftp';
 
 
-const Applications = () => {  
+const Applications: React.FC = () => {  
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = (credentials: any) => {
+    // Aquí puedes realizar la lógica de autenticación (por ahora, simplemente marcamos como autenticado)
+    setLoggedIn(true);
+  };
+
+  
     return (
         <div className="App-Applications">
-          <Chat />
+          {isLoggedIn ? (
+            <Chat />
+          ) : (
+            <Login onLogin={handleLogin} />
+          )}
           {/* <Sftp /> */}
         </div>
     );
